31937f1a64e29d2cccd5dfb0065c83635ed002d5
basemEk <basem.kreidly@gmail.com>
2020-01-20 09:34:37 +0200

    changing the App name

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,22 @@
+"SELECT Name FROM students"
+
+"SELECT * FROM students WHERE Age > 30"
+
+"SELECT name FROM students WHERE Gender = 'F' AND Age > 30"
+
+"SELECT Points FROM students WHERE name = 'Alex'"
+
+"INSERT INTO students(ID, Name, Age, Gender, Points )
+VALUES (NULL, 'BASEM', '30', 'M', '130')
+select * from students"
+
+"UPDATE students
+SET Points = '500'
+WHERE ID = 2;"
+
+"UPDATE students
+SET Points = '190'
+WHERE ID = 1;"
+
+
+
