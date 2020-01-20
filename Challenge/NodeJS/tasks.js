ce53201ff7b129eab0cf3bb1cb29fd1d753f850a
basemEk <basem.kreidly@gmail.com>
2020-01-20 12:57:51 +0200

    handled empty state

--- Challenge/NodeJS/tasks.js
+++ Challenge/NodeJS/tasks.js
@@ -62,7 +62,9 @@ function unknownCommand(c) {
  * @returns {void}
  */
 function hello(name) {
-  console.log("hello " + name + "!");
+  if (name == "") {
+    console.log("hello!");
+  } else console.log("hello " + name + "!");
 }
 
 /**
