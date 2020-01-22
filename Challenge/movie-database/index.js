685e799386a7d284c61e1160b425d576edb295c0
basemEk <basem.kreidly@gmail.com>
2020-01-22 23:38:29 +0200

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) =>
-  res.send("Hello World! today we will going to sove node question")
-);
+app.get("/test", (req, res) => res.send("status:200, message:'ok'"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(2020);
+
+app.get("/time", function(req, res) {
+  res.send("status:200, message:");
+});
