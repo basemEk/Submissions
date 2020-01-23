207216ae8bed9cb1dcf71d901cc1eaae6ff6d3a2
basemEk <basem.kreidly@gmail.com>
2020-01-23 11:00:04 +0200

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.listen(2020);
 
 app.get("/", (req, res) => res.send("hello"));
 
@@ -14,4 +15,20 @@ app.get("/time", (req, res) => {
   });
 });
 
-app.listen(2020);
+app.get("/hello/:ID", (req, res) => {
+  // console.log(req.params,req.query);
+  res.json({ status: 200, message: "Hello " + req.params.ID });
+});
+
+app.get("/search", (req, res) => {
+  // console.log(req.params,req.query);
+  if (req.query.s) {
+    res.json({ status: 200, message: "ok ", data: req.query.s });
+  } else {
+    res.json({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+  }
+});
