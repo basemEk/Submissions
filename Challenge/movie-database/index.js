c7b621161f10e44ef166b7d6324b0ea02a0e9e4b
basemEk <basem.kreidly@gmail.com>
2020-01-24 18:28:21 +0200

    step 7

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,7 +5,10 @@ app.listen(2020);
 app.get("/", (req, res) => res.send("hello"));
 
 app.get("/test", (req, res) => {
-  res.send({ status: 200, message: "ok" });
+  res.send({
+    status: 200,
+    message: "ok"
+  });
 });
 
 app.get("/time", (req, res) => {
@@ -17,12 +20,19 @@ app.get("/time", (req, res) => {
 
 app.get("/hello/:ID", (req, res) => {
   // console.log(req.params,req.query);
-  res.json({ status: 200, message: "Hello " + req.params.ID });
+  res.json({
+    status: 200,
+    message: "Hello " + req.params.ID
+  });
 });
 
 app.get("/search", (req, res) => {
   if (req.query.s) {
-    res.json({ status: 200, message: "ok ", data: req.query.s });
+    res.json({
+      status: 200,
+      message: "ok ",
+      data: req.query.s
+    });
   } else {
     res.json({
       status: 500,
@@ -33,34 +43,68 @@ app.get("/search", (req, res) => {
 });
 
 const movies = [
-  { title: "Jaws", year: 1975, rating: 8 },
-  { title: "Avatar", year: 2009, rating: 7.8 },
-  { title: "Brazil", year: 1985, rating: 8 },
-  { title: "الارهاب والكباب", year: 1992, rating: 6.2 }
+  {
+    title: "Jaws",
+    year: 1975,
+    rating: 8
+  },
+  {
+    title: "Avatar",
+    year: 2009,
+    rating: 7.8
+  },
+  {
+    title: "Brazil",
+    year: 1985,
+    rating: 8
+  },
+  {
+    title: "الارهاب والكباب",
+    year: 1992,
+    rating: 6.2
+  }
 ];
 
 app.get("/movies/create", (req, res) => {
-  res.json({ status: 200, data: "creating movies" });
+  res.json({
+    status: 200,
+    data: "creating movies"
+  });
 });
 
 app.get("/movies/read", (req, res) => {
-  res.json({ status: 200, data: "read movies" });
+  res.json({
+    status: 200,
+    data: "read movies"
+  });
 });
 
 app.get("/movies/update", (req, res) => {
-  res.json({ status: 200, data: "update movies" });
+  res.json({
+    status: 200,
+    data: "update movies"
+  });
 });
 
 app.get("/movies/delete", (req, res) => {
-  res.json({ status: 200, data: "delete movies" });
+  res.json({
+    status: 200,
+    data: "delete movies"
+  });
 });
 
 app.get("/movies/read/by-date", (req, res) => {
-  res.json({ status: 200, data: movies.sort((a, b) => a.year - b.year) });
+  res.json({
+    status: 200,
+    data: movies.sort((a, b) => a.year - b.year)
+  });
 });
 
 app.get("/movies/read/by-rating", (req, res) => {
-  res.json({ status: 200, data: movies.sort((a, b) => a.rating - b.rating) });
+  res.json({
+    status: 200,
+    data: movies.sort((a, b) => a.rating - b.rating)
+  });
 });
 
 app.get("/movies/read/by-title", (req, res) => {
@@ -68,4 +112,19 @@ app.get("/movies/read/by-title", (req, res) => {
     status: 200,
     data: movies.sort((a, b) => a.title.localeCompare(b.title))
   });
+});
+
+app.get("/movies/read/id/:id", (req, res) => {
+  if (req.params.id < movies.length) {
+    res.send({
+      status: 200,
+      data: movies[parseInt(req.params.id)]
+    });
+  } else {
+    res.send({
+      status: 404,
+      error: true,
+      message: "the movie" + req.params.id + "does not exist"
+    });
+  }
 });
